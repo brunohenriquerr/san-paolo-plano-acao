a43cc0a1e222b77c42cf0ee6c5d8873a6eb792e2
brunohenriquerr <brunohenriqueocc@gmail.com>
2026-06-01 06:36:09 -0300

    feat: roteamento /view para modo leitura

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import Viewer from './Viewer.jsx'
+
+const path = window.location.pathname;
+const Component = path.startsWith('/view') ? Viewer : App;
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Component />
   </React.StrictMode>
 )
